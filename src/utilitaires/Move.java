44344f3da1303b00968f0184fab96998514f03e4
czadowski <maksymilian.czadowski@ynov.com>
2021-11-23 15:54:28 +0100

    add coordinate and move

---
+++ src/utilitaires/Move.java
@@ -0,0 +1,22 @@
+package utilitaires;
+
+import model.Coordinate;
+import model.Pion;
+
+import java.util.ArrayList;
+
+public class Move {
+    public static void MovePion(int ch, char[][] plateau, ArrayList<Pion> allPion) {
+        if(canMove(ch,plateau,allPion)){
+            System.out.println("ok");
+        }
+    }
+
+    public static boolean canMove(int ch, char[][] plateau, ArrayList<Pion> allPion){
+        Coordinate co = new Coordinate();
+        Utilitaires.transformToCoordinate(ch, plateau, co);
+        System.out.println("y: " + co.getY() + "\nx: " + co.getX());
+        return true;
+    }
+
+}
